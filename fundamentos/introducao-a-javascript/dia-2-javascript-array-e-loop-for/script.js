5f8ff4a98a990794700debfa47e86ffca3de8a62
edvansabino <edvansabino@gmail.com>
2022-09-22 19:09:12 -0300

    Desenvolvendo exercícios até linha 5

--- fundamentos/introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
+++ fundamentos/introducao-a-javascript/dia-2-javascript-array-e-loop-for/script.js
@@ -4,9 +4,18 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let sum = 0;
 let mediaNumbers = 0;
+let message = "";
 
 for (let i = 0; i < numbers.length; i += 1) {
-    sum += numbers[i];
-    mediaNumbers = sum / numbers.length;
+  sum += numbers[i];
+  mediaNumbers = sum / numbers.length;
 }
+
+if (mediaNumbers > 20) {
+  message = "valor maior que 20.";
+} else {
+  message = "valor menor ou igual a 20.";
+}
+
 console.log(mediaNumbers);
+console.log(message);
