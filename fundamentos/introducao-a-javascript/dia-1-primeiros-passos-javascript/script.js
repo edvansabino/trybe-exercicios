26912b10021d58156f416b3d4c15062f8b9ee4cf
edvansabino <edvansabino@gmail.com>
2022-09-24 17:12:31 -0300

    Alteração dia 1 javascript

---
+++ fundamentos/introducao-a-javascript/dia-1-primeiros-passos-javascript/script.js
@@ -0,0 +1,11 @@
+//Ex-1.dia-1
+const myName = 'Edvan';
+const birthCity = 'São Paulo';
+let birthYear = 1980;
+
+birthYear = 22;
+//birthCity = 'Tokyo'; *é uma constante e seu valor não pode ser alterado.
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
