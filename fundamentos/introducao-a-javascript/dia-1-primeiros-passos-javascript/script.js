941b0f28aa77fc8b04bebb07eb792a2fcc73ed79
edvansabino <edvansabino@gmail.com>
2022-09-24 18:12:44 -0300

    Exercicio 3

--- fundamentos/introducao-a-javascript/dia-1-primeiros-passos-javascript/script.js
+++ fundamentos/introducao-a-javascript/dia-1-primeiros-passos-javascript/script.js
@@ -1,5 +1,5 @@
 //Ex-1.dia-1
-const myName = 'Edvan';
+/*const myName = 'Edvan';
 const birthCity = 'São Paulo';
 let birthYear = 1980;
 
@@ -8,4 +8,22 @@ birthYear = 22;
 
 console.log(myName);
 console.log(birthCity);
-console.log(birthYear);
+console.log(birthYear);*/
+
+/*const base = 5;
+const heigth = 8;
+const area = base * heigth;
+const perimeter = base + heigth + base + heigth;
+
+console.log(area);
+console.log(perimeter);*/
+
+const nota = 85;
+
+if (nota >= 80) {
+  console.log("Parabéns, você foi aprovada(o)!");
+} else if (nota < 80 && nota >= 60){
+  console.log("Você está na nossa lista de espera");
+} else {
+  console.log("Você foi reprovada(o)");
+}
