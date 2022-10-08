66634374acb13fb95340deb7f2e1279aa3b094a5
edvansabino <edvansabino@gmail.com>
2022-10-08 18:29:29 -0300

    Adicionando Modificações 08-10-2022

--- fundamentos/introducao-a-javascript/dia-1-primeiros-passos-javascript/exercicios-de-fixacao/ex-fix-1.js
+++
@@ -1,9 +0,0 @@
-const myName = 'Edvan'
-const birthCity = 'São Paulo'
-let birthYear = 1980
-birthYear = 2030
-
-
-console.log(myName)
-console.log(birthCity)
-console.log(birthYear)
--- fundamentos/introducao-a-javascript/dia-1-primeiros-passos-javascript/exercicios-de-fixacao/ex-fix-3.js
+++
@@ -1,9 +0,0 @@
-const notaFinal = 81;
-
-if (notaFinal >= 80) {
-  console.log('Parabéns, você foi aprovada(o)!');
-} else if (notaFinal < 80 && notaFinal >= 60) {
-  console.log('Você está na nossa lista de espera');
-} else {
-  console.log('Você foi reprovada(o)');
-}
